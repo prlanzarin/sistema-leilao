b1cac632a995e7df44f8db9620640d0738820c91
lpsilvestrin <lpsilvestrin@inf.ufrgs.br>
2015-05-17 15:26:44 -0300

    idKey adicionado

--- src/business/entities/Property.scala
+++ src/business/entities/Property.scala
@@ -3,10 +3,12 @@ package business.entities;
 import PropertyKind._
 
 class Property(
+  private var _idKey,
   private var _name: String,
   private var _value: Double,
   private var _kind: PropertyKind) {
 
+  def idKey = this._idKey
   def name = this._name
   def value = this._value
   def kind = this._kind
