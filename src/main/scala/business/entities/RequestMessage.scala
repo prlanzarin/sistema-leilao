51792a494cdf2a999bfc9a16e61d35464156f289
Ricardo G. Herdt <rgherdt@inf.ufrgs.br>
2015-05-17 20:22:52 -0300

    AddPropertyRequest com dois params

--- src/main/scala/business/entities/RequestMessage.scala
+++ src/main/scala/business/entities/RequestMessage.scala
@@ -1,6 +1,6 @@
 package business.entities
 
 abstract class RequestMessage extends Serializable
-case class AddIndebtedRequest(indebted : Indebted) extends RequestMessage
-case class AddPropertyRequest(property : Property) extends RequestMessage
+case class AddIndebtedRequest(i: Indebted) extends RequestMessage
+case class AddPropertyRequest(i: Indebted, p : Property) extends RequestMessage
 
