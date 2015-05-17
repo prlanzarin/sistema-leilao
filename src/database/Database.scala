881413b50a699b90564776626d024a99be711d0e
prlanzarin <prlanzarin@inf.ufrgs.br>
2015-05-16 21:17:33 -0300

    Adicionei protótipos das funções de Database para que possam ser chamadas no service

--- src/database/Database.scala
+++ src/database/Database.scala
@@ -1,5 +1,75 @@
 package database
 
-class Database {
+import java.util.Date
+import business.entities.PropertyKind
 
+class Database {
+  
+  /*
+   * TODO / 2nd iteration
+   */
+  def queryScheduledAuctions = {
+    
+  }
+  /*
+   * TODO / 2nd iteration
+   */
+  def queryOpenAuctions = {
+    
+  }
+  /*
+   * TODO / 2nd iteration
+   */
+  def queryClosedAuctions = {
+    
+  }
+  /*
+   * TODO
+   */
+  def queryIndebteds = {
+    
+  }
+  /*
+   * TODO
+   */
+  def queryProperties = {
+    
+  }
+  /*
+   * TODO
+   */
+  def addIndebted(name : String, birthDay : Date, debt : Double, CPF : String) = {
+    
+  }
+  /*
+   * TODO
+   */
+  def addProperty(propertyName : String, value : Double, kind : String) = {
+    
+  }
+  /*
+   * TODO / 2nd iteration
+   */
+  def addAuction() = {
+    
+  }
+  /*
+   * TODO / 2nd iteration
+   */
+  def addUser() = {
+    
+  }
+  /* 
+   * TODO
+   * type String of CPF is a placeholder
+   */
+  def queryProperty(indebtedCPF : String, propertyName : String) = {
+    
+  }
+  /*
+   * TODO
+   */
+  def queryIndebted(indebtedCPF : String) = {
+    
+  }
 }
