972361da3203e0212f685a0a05f693b45a4222ae
lpsilvestrin <lpsilvestrin@inf.ufrgs.br>
2015-05-17 11:35:45 -0300

    atributo cpf do indebted adicionado

--- src/business/entities/Indebted.scala
+++ src/business/entities/Indebted.scala
@@ -5,13 +5,15 @@ import java.util.Date
 class Indebted(
   private var _name: String,
   private var _birthDay: Date,
-  private var _debt: Double) {
+  private var _debt: Double,
+  private var _cpf: String) {
   
   private var _properties : List[Property] = List[Property]()
   
   def name = this._name
   def birthDay = this._birthDay
   def debt = this._debt
+  def cpf = this._cpf
   def properties = this._properties
   
 }
