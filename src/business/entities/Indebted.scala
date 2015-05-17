4e3a920556b1195a9fa3d667cc94cbd54b705058
Ricardo G. Herdt <rgherdt@inf.ufrgs.br>
2015-05-17 16:14:13 -0300

    val in params

--- src/business/entities/Indebted.scala
+++ src/business/entities/Indebted.scala
@@ -2,10 +2,11 @@ package business.entities;
 
 import java.util.Date
 
-class Indebted(name: String,
-               birthDay: Date,
-               dept: Double,
-               cpf: String) {
+class Indebted (val name: String,
+                val birthDay: Date,
+                val dept: Double,
+                val cpf: String)
+extends Serializable {
     private var properties: List[Property] = List.empty[Property]
     def addProperty(p: Property) = p :: this.properties
 }
