398b3ab11fc6cad2046ab528a90cb9ca11501fb3
Ricardo G. Herdt <rgherdt@inf.ufrgs.br>
2015-05-17 22:38:54 -0300

    removi comentários TODO, tao nos sys.error

--- src/main/scala/database/Database.scala
+++ src/main/scala/database/Database.scala
@@ -5,66 +5,40 @@ import business.entities._
 
 class Database {
   
-  /*
-   * TODO / 2nd iteration
-   */
   def queryScheduledAuctions = {
     
   }
-  /*
-   * TODO / 2nd iteration
-   */
+
   def queryOpenAuctions = {
     
   }
-  /*
-   * TODO / 2nd iteration
-   */
+
   def queryClosedAuctions = {
     
   }
-  /*
-   * TODO
-   */
+
   def queryIndebteds() : List[Indebted]= sys.error("todo")
     
-  /*
-   * TODO
-   */
   def queryProperties() : List[Property] = sys.error("todo")
-  /*
-   * TODO
-   */
+
   def addIndebted(name : String, birthDay : Date, debt : Double, CPF : String) : Unit = {
     
   }
-  /*
-   * TODO
-   */
+
   def addProperty(cpf : String, propertyName : String, value : Double, kind : String) : Unit = {
     
   }
-  /*
-   * TODO / 2nd iteration
-   */
+
   def addAuction() = {
     
   }
-  /*
-   * TODO / 2nd iteration
-   */
+
   def addUser() = {
     
   }
-  /* 
-   * TODO
-   * type String of CPF is a placeholder
-   */
+
   def queryProperty(indebtedCPF : String, propertyName : String) : Boolean = sys.error("todo")
     
-  /*
-   * TODO
-   */
   def queryIndebted(indebtedCPF : String) : Boolean = sys.error("todo")
     
 }
