67a90352a5dc75fef8cb04d348f8e2ad15b4467f
prlanzarin <prlanzarin@inf.ufrgs.br>
2015-05-26 00:58:47 -0300

    Arrumando resquicios do maior merge ja visto

--- src/main/scala/business/entities/Bid.scala
+++ src/main/scala/business/entities/Bid.scala
@@ -4,18 +4,8 @@ import presentation.ui.UIUtils
 
 case class Bid( auction: Long,
                 client: Client,
-                value : Double)
-{
+                value : Double) {
+
     override def toString: String = "Cliente: " + client.name + "\nValor: " +
         value
 }
-
-
-/*
-private var _value: Double = 0
-
-    def value = _value
-
-    def value_(nvalue : Double) = _value = nvalue
-
- */
--- src/main/scala/business/entities/User.scala
+++ src/main/scala/business/entities/User.scala
@@ -1,13 +1,9 @@
 package business.entities
 
-<<<<<<< HEAD
-class User( val userName: String,
-            val password: String,
-            val name: String) {
-=======
+
 class User(   val userName: String,
               val password: String,
-              val name: String) extends Serializable{
->>>>>>> b8470302a918e52d996a02a6564a665366598a8f
+              val name: String)
+    extends Serializable {
 
 }
